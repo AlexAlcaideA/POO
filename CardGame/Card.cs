e9f7915ec64e2893be56e01b4ed0403b3d486e0e
Alex Alacaide Arroyes <alex.alcaide@enti.cat>
2024-09-17 12:43:35 +0200

    Created class card

---
+++ CardGame/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    internal class Card
+    {
+        public enum ECardType
+        {
+            Spades,
+            Hearts,
+            Diamonds,
+            Clubs
+        }
+
+        private int num;
+        private ECardType cardType;
+
+        public int NUM { get { return num; } }
+        public ECardType CardType { get { return cardType; } }
+
+        public Card() 
+        { 
+            num = 0;
+            cardType = ECardType.Spades;
+        }
+
+        public Card(int _num, ECardType _cardType)
+        {
+            num = _num;
+            cardType = _cardType;
+        }
+    }
+}
